204cbf3e794f53a4152f189f54991786a71c8a5f
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-12-21 13:43:51 +0600

    Resume Link Updated

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className='flex flex-col justify-center items-center relative'>
             <div><img src={banner} alt="Banner"/></div>
-            <div className='absolute bottom-64'><button className='btn btn-outline btn-info'><a href="https://drive.google.com/file/d/1NyNW5iEviQmKATvrFXMitC-WjwNDTNax/view?usp=drive_link" target='blank' download> Download Resume</a></button></div>
+            <div className='absolute bottom-64'><button className='btn btn-outline btn-info'><a href="https://drive.google.com/file/d/1_8Q1km4ZrGudnBejmTkpF1jbMA-5EC9w/view?usp=drive_link" target='blank' download> Download Resume</a></button></div>
         </div>
     );
 };
